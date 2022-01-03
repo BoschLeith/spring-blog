9a2873b7c4311d80fcc8023795eff842c3cc8481
Bosch Leith <bosch.leith@gmail.com>
2022-01-03 15:22:04 -0600

    Added: post views

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,24 +1,34 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
 	@GetMapping("/posts")
-	@ResponseBody
-	public String viewPosts(){
-		return "posts index page";
+	public String viewPosts(Model model){
+		Post post1 = new Post("Title1", "Body1");
+		Post post2 = new Post("Title2", "Body2");
+		ArrayList<Post> posts = new ArrayList<Post>();
+		posts.add(post1);
+		posts.add(post2);
+		model.addAttribute("Posts", posts);
+		return "posts/index";
 	}
 
 	@GetMapping("/posts/{id}")
-	@ResponseBody
-	public String viewPostById(@PathVariable int id){
-		return "view an individual post for postId " + id;
+	public String viewPostById(@PathVariable int id, Model model){
+		Post post = new Post("Test Title", "Test Body");
+		model.addAttribute("Post", post);
+		return "posts/show";
 	}
 
 	@GetMapping("/posts/create")
