0c347e459148e9478183fb57cf1003d2bcf2c378
Bosch Leith <bosch.leith@gmail.com>
2022-01-03 16:07:34 -0600

    Refactor: readability

--- src/main/java/com/codeup/springblog/controllers/DiceController.java
+++ src/main/java/com/codeup/springblog/controllers/DiceController.java
@@ -9,30 +9,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DiceController {
 
-	public static double getRandomRoll(double min, double max){
-		double randomRoll = (int)(Math.random()*((max-min)+1))+min;
-		return randomRoll;
+	public static double getRandomRoll(double min, double max) {
+		return (int) (Math.random() * ((max - min) + 1)) + min;
 	}
 
 	@GetMapping("/roll-dice")
-	public String showRollDice(){
+	public String showRollDice() {
 		return "roll-dice";
 	}
 
 	@PostMapping("/roll-dice")
 	public String guessRoll(@RequestParam(name = "userGuess") String userGuess, Model model) {
-		model.addAttribute("userGuess", "User guess is " + userGuess);
-		int randomRoll = (int) getRandomRoll(1, 6);
-		model.addAttribute("randomRoll", "Random roll is " + randomRoll);
 		int userInt = Integer.parseInt(userGuess);
-		if (userInt == randomRoll) {
-			System.out.println("Guess and Roll the same");
-			model.addAttribute("correct", "You guess the same number!");
-			return "roll-dice";
-		}
+		int randomRoll = (int) getRandomRoll(1, 6);
+
 		System.out.println(userGuess);
 		System.out.println(randomRoll);
-		model.addAttribute("correct", "You guess the wrong number!");
+
+		model.addAttribute("userGuess", "User guess is " + userGuess);
+		model.addAttribute("randomRoll", "Random roll is " + randomRoll);
+
+		if (userInt == randomRoll) {
+			System.out.println("userGuess and randomRoll the same");
+			model.addAttribute("message", "You guess the same number!");
+			return "roll-dice";
+		}
+
+		model.addAttribute("message", "You guess the wrong number!");
 		return "roll-dice";
 	}
 
