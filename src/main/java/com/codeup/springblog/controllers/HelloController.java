692bd48f4793df11d4e00b7740f673a42c856348
Bosch Leith <bosch.leith@gmail.com>
2022-01-03 12:29:17 -0600

    Added: join view and controller

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -22,6 +23,17 @@ public class HelloController {
 	@ResponseBody
 	public String addOne(@PathVariable int number){
 		return "Your number plus one is " + (number + 1);
+	}
+
+	@GetMapping("/join")
+	public String showJoinForm() {
+		return "join";
+	}
+
+	@PostMapping("/join")
+	public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+		model.addAttribute("cohort", "Welcome to " + cohort + "!");
+		return "join";
 	}
 
 }
